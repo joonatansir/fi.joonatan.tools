f2072edd68e3fbdf9450f95c528603822343ea58
joonatan <joonatan@smallgiantgames.com>
2025-09-04 16:35:22 +0300

    Add SpringMove

--- Runtime/Tools.cs
+++ Runtime/Tools.cs
@@ -13,6 +13,53 @@ namespace Joonatan
         {
             return Mathf.Sin(x) / (0.4f + 0.4f * Mathf.Abs(Mathf.Sin(x)));
         }
+        
+        public static void SpringMove(ref float x, ref float v, float xt, float zeta, float omega, float h)
+        {
+            /*
+             *
+             * https://allenchou.net/2015/04/game-math-precise-control-over-numeric-springing/
+             *
+             *  x       - value
+             *  v       - velocity
+             *  xt      - target value
+             *  zeta    - damping ratio
+             *  omega   - oscillation frequency
+             *  h       - timestep
+             */
+            var f = 1.0f + 2.0f * h * zeta * omega;
+            var oo = omega * omega;
+            var hhoo = h * h * oo;
+            var d = 1.0f / (f + hhoo);
+            var numX = f * x + h * v + hhoo * xt;
+            var numV = v + h * oo * (xt - x);
+            x = numX * d;
+            v = numV * d;
+        }
+
+        public static void SpringMove(ref Vector2 x, ref Vector2 v, Vector2 xt, float zeta, float omega, float h)
+        {
+            var f = 1.0f + 2.0f * h * zeta * omega;
+            var oo = omega * omega;
+            var hhoo = h * h * oo;
+            var d = 1.0f / (f + hhoo);
+            var numX = f * x + h * v + hhoo * xt;
+            var numV = v + h * oo * (xt - x);
+            x = numX * d;
+            v = numV * d;
+        }
+
+        public static void SpringMove(ref Vector3 x, ref Vector3 v, Vector3 xt, float zeta, float omega, float h)
+        {
+            var f = 1.0f + 2.0f * h * zeta * omega;
+            var oo = omega * omega;
+            var hhoo = h * h * oo;
+            var d = 1.0f / (f + hhoo);
+            var numX = f * x + h * v + hhoo * xt;
+            var numV = v + h * oo * (xt - x);
+            x = numX * d;
+            v = numV * d;
+        }
     }
 }
 
